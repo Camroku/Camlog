0f63528ade20bba188c82e69ca153ac7de4e4634
Camroku <cinaryilmaz.gnu@gmail.com>
2022-07-08 19:24:18 +0300

    Add block code + fix bugs

--- src/qomar.py
+++ src/qomar.py
@@ -7,7 +7,7 @@ qolang_export = {
 # Some functions were taken from QoLang's lexer.
 class QomarCompiler:
     def __init__(self, text):
-        self.text = text.replace("\n\n", "</p><p>").replace("\n", "<br>")
+        self.text = text
         self.pos = 0
         self.current_char = self.text[self.pos]
         self.out = ""
@@ -19,8 +19,8 @@ class QomarCompiler:
         else:
             self.current_char = self.text[self.pos]
 
-    def peek(self):
-        peek_pos = self.pos + 1
+    def peek(self, characters=1):
+        peek_pos = self.pos + characters
         if peek_pos > len(self.text) - 1:
             return None
         else:
@@ -50,12 +50,49 @@ class QomarCompiler:
         
         self.out += "</code>"
 
+    def blockcode(self):
+        self.advance()
+        self.advance()
+        self.advance()
+        escaped = False
+        self.out += "<pre>"
+        while True:
+            if escaped:
+                self.out += self.current_char
+            else:
+                if self.current_char == '`' and self.peek() == '`' and self.peek(2) == '`':
+                    break
+                else:
+                    self.out += self.current_char
+            self.advance()
+        
+        self.advance()
+        self.advance()
+        self.out += "</pre>"
+
+    def skipspace(self):
+        while self.current_char != None and self.current_char.isspace():
+            if self.current_char == '\n' and self.peek() == '\n':
+                self.out += "</p><p>"
+                self.advance()
+            elif self.current_char == '\n':
+                self.out += '<br>'
+            self.advance()
+
+        if self.peek(-1) != '\n':
+            self.out += " "
+
     def compile(self):
         escaped = False
         while self.current_char is not None:
+            if self.current_char.isspace():
+                self.skipspace()
+                continue
             if not escaped:
                 if self.current_char == '/' and self.peek() == '*':
                     self.skipcomment()
+                elif self.current_char == '`' and self.peek() == '`' and self.peek(2) == '`':
+                    self.blockcode()
                 elif self.current_char == '`':
                     self.code()
                 elif self.current_char == '\\':
